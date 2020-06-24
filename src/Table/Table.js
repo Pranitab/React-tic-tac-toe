0a950294a8c7bf5a5dc0e582fb28863484e28daa
Pranitab <Pranitabhagat91@gmail.com>
2020-06-24 16:55:41 +0530

    warnings removed

--- src/Table/Table.js
+++ src/Table/Table.js
@@ -2,18 +2,15 @@ import React from 'react';
 import TableRowComp from './TableRowComp/TableRowComp';
 import './Table.css';
 var playerWinData=[
-    ['0','1','2'],
-    ['3','4','5'],
-    ['6','7','8'],
-    ['0','3','6'],
-    ['1','4','7'],
-    ['2','5','8'],
-    ['0','4','8'],
-    ['2','4','6'],
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
 ];
-var player1Data=[];
-var player2Data=[];
-var winnerBlock;
 class Table extends React.Component{
     constructor(){
         super();
@@ -23,7 +20,9 @@ class Table extends React.Component{
             winner:null,
             drawMatch:false,
             tableCell:Array(9).fill(null),
-            winningCell:Array(9).fill(null)
+            winningCell:Array(9).fill(null),
+            player1Data:[],
+            player2Data:[]
         }
     }
     onResetClick=()=>{
@@ -33,11 +32,10 @@ class Table extends React.Component{
             winner:null,
             drawMatch:false,
             tableCell:Array(9).fill(null),
-            winningCell:Array(9).fill(null)
+            winningCell:Array(9).fill(null),
+            player1Data:[],
+            player2Data:[]
         })
-        player1Data=[];
-        player2Data=[];
-        winnerBlock=null;
     }
   
     onCellClick=(e)=>{
@@ -51,61 +49,73 @@ class Table extends React.Component{
         
 
         if(this.state.player1){
-            
-            player1Data.push(e.target.getAttribute('id'));
-            if(player1Data.length >= 3)
+            this.setState(prevState=>({player1Data:prevState.player1Data.concat(currCell)}));
+        }
+        else if(this.state.player2){
+            this.setState(prevState=>({player2Data:prevState.player2Data.concat(currCell)}));
+        }
+        
+    }
+    componentDidUpdate(){
+        
+        let winnerBlock;
+        if(this.state.player1Data.length >= 3 && !this.state.winner && !this.state.player1 && !this.state.drawMatch) 
             {
+                let curScope = this;
                    for(let i=0;i<playerWinData.length;i++)
                    {
                         let is_same = playerWinData[i].every(function(element, index) {
-                            return player1Data.includes(element);
+                            return curScope.state.player1Data.includes(element);
                         });
                         if(is_same){
                             console.log('Player 1 win',i);
                             winnerBlock = i;
                             this.setState({winner:'Player 1'});
+                            for(let i=0;i<playerWinData[winnerBlock].length;i++){
+                                this.state.winningCell.splice(playerWinData[winnerBlock][i],1,' winnerBlock')
+                                   this.setState(prevState=>({
+                                       winningCell:this.state.winningCell
+                                   }))
+               
+                        }
                             break;
                         }
                         else{
-                            if(player1Data.length === 5)
+                            if(curScope.state.player1Data.length === 5)
                             {
+                                console.log('draw match...!!!')
                                 this.setState({drawMatch:true});
                             }
                         }
                     }
                 
             }
-        }
-        else if(this.state.player2){
-            
-            player2Data.push(e.target.getAttribute('id'));
-            if(player2Data.length >= 3)
+            else if(this.state.player2Data.length >= 3 && !this.state.winner && !this.state.player2&& !this.state.drawMatch)
             {
+                let curScope = this;
                 for(let i=0;i<playerWinData.length;i++)
                    {
                         let is_same = playerWinData[i].every(function(element, index) {
-                            return player2Data.includes(element); 
+                            return curScope.state.player2Data.includes(element); 
                         });
                         if(is_same){
                             console.log('Player 2 win',i);
                             winnerBlock = i;
-                            this.setState({winner:'Player 2'});
+                            curScope.setState({winner:'Player 2'});
+
+                            for(let i=0;i<playerWinData[winnerBlock].length;i++){
+                                this.state.winningCell.splice(playerWinData[winnerBlock][i],1,' winnerBlock')
+                                   this.setState(prevState=>({
+                                       winningCell:this.state.winningCell
+                                   }))
+               
+                                }
                             break;
                         }
                     }
             }
-        }
-        if(winnerBlock || winnerBlock===0){
-            playerWinData[winnerBlock].forEach((e,i)=>{  
-                 this.state.winningCell.splice(playerWinData[winnerBlock][i],1,' winnerBlock')
-                    this.setState(prevState=>({
-                        winningCell:this.state.winningCell
-                    }))
-
-             })
-        }
+        
     }
-    
     render(){
         return (
         <React.Fragment>
